06cdbddea02e614a20cf53ae3b4ecbf82ca7cef9
lpettit1 <50005432+lpettit1@users.noreply.github.com>
2019-11-13 21:33:43 -0700

    add onto the friends api

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ var friends = [
     {
         name: "Edward Connolly",
         photo: "./images/edward.jpg",
-        scores: ["1", "1", "1", "1", "1"]
+        scores: ["1", "1", "1", "1", "1", "3", "3", "3", "3", "3"]
     },
     {
         name: "John Reed",
         photo: "./images/john.jpg",
-        scores: ["3", "3", "3", "3", "3"]   
+        scores: ["3", "3", "3", "3", "3", "4", "4", "4", "4", "4"]   
     },
     {
         name: "Charles Knox",
         photo: "./images/charles.jpg",
-        scores: ["4", "4", "4", "4", "4"]
+        scores: ["4", "4", "4", "4", "4", "2", "2", "2", "2", "2"]
     },
     {
         name: "William Clements",
         photo: "./images/william.jpg",
-        scores: ["2", "2", "2", "2", "2"]  
+        scores: ["2", "2", "2", "2", "2", "1", "1", "1", "1", "1"]  
     }
 ];
 
